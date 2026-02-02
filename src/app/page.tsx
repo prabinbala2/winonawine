dab9236acac9401d5971b4c2c0560c93eb60a115
prabinbala2 <92999053+prabinbala2@users.noreply.github.com>
2026-02-02 14:07:46 +0530

    Menu error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ import OvalCtaBanner from "@/components/OvalCtaBanner";
 import StoreLocations from "@/components/StoreLocations";
 import Footer from "@/components/common/Footer";
 
+import type { Product } from "@/components/SectionGrid";
+
 import Image from "next/image";
 
 
